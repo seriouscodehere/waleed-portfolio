495b371898bba884173c6e0dd8a7953c80936a29
seriouscodehere <mwaleedwik1@gmail.com>
2026-01-18 22:00:14 +0500

    working

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/React-Portfolio/",  // add this line
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
