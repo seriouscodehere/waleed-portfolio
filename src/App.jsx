ca46199951f03c714d57d1b1a93daad3f00ead4e
seriouscodehere <mwaleedwik1@gmail.com>
2026-01-18 17:58:27 +0500

    cinfug updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
 import WelcomeScreen from "@/components/WelcomeScreen";
-import { Analytics } from "@vercel/analytics/react"; 
+// import { Analytics } from "@vercel/analytics/react"; 
 
 function App() {
   const [welcomeComplete, setWelcomeComplete] = useState(false);
@@ -26,7 +26,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-          <Analytics />
+          {/* <Analytics /> */}
         </BrowserRouter>
       )}
     </ThemeProvider>
